bb3d8ff1046131491bd1d6a53d09fc6f44346c8d
SOLO-EAGLE <eagle1bluestorm@gmail.com>
2022-12-01 16:25:01 +0100

    Ajout tests 3.1 et triggers

--- actions/action_fct_partie_3_1.py
+++ actions/action_fct_partie_3_1.py
@@ -11,23 +11,3 @@ class AppFctPartie3_1(QDialog):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_partie3_1.ui", self)
         self.data = data
-
-    def refreshResult(self):
-
-        display.refreshLabel(self.ui.label_fct_partie3_1, "")
-        if not self.ui.lineEdit.text().strip():
-            self.ui.table_fct_partie3_1.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_partie3_1, "Veuillez indiquer un numéro d'équipe ou de sportif")
-        else:
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute(
-                    "INSERT INTO LesInscriptions VALUES(",
-                    [self.ui.lineEdit.text().strip()])
-            except Exception as e:
-                self.ui.table_fct_partie3_1.setRowCount(0)
-                display.refreshLabel(self.ui.label_fct_partie3_1, "Impossible d'afficher les résultats : " + repr(e))
-            else:
-                i = display.refreshGenericData(self.ui.table_fct_partie3_1, result)
-                if i == 0:
-                    display.refreshLabel(self.ui.label_fct_partie3_1, "Aucun résultat")
