f93aa43f0650fb90f74e41c5c76eb4077c9bcbf1
Killian Cengo <cengokillian@hotmail.com>
2022-12-02 16:01:04 +0100

    partie 3.2 OK ! (besoin de triggers pour les parties 3.1 et 3.2)

--- main.py
+++ main.py
@@ -197,14 +197,12 @@ class AppWindow(QMainWindow):
         self.fct_partie2_2_dialog.show()
     # TODO 3 : ajouter la définition des méthodes déclenchées lors des clicks sur les boutons de la partie 3
     def open_fct_partie3_1(self):
-
         if self.fct_partie3_1_dialog is not None:
             self.fct_partie3_1_dialog.close()
         self.fct_partie3_1_dialog = AppFctPartie3_1(self.data)
         self.fct_partie3_1_dialog.show()
 
     def open_fct_partie3_2(self):
-
         if self.fct_partie3_2_dialog is not None:
             self.fct_partie3_2_dialog.close()
         self.fct_partie3_2_dialog = AppFctPartie3_2(self.data)
