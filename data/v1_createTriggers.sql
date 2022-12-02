b5d706500fd222fe6f9be954a8892b6dde64efd1
SOLO-EAGLE <eagle1bluestorm@gmail.com>
2022-12-02 14:57:48 +0100

    ajout de 2 triggers qui marchent sur DB Browser

--- data/v1_createTriggers.sql
+++ data/v1_createTriggers.sql
@@ -1,8 +1,16 @@
--- TODO 3.3 Créer un trigger pertinent
---CREATE TRIGGER IF NOT EXISTS InsertCheck1
-   -- BEFORE INSERT ON LesInscriptions
-   -- WHEN EXISTS (NEW.numIn NOT IN(SELECT numSp FROM LesSportifsEQ) OR NEW.numIn NOT IN(SELECT numEq FROM LesSportifsEQ))
-  --  BEGIN
-   --     SELECT RAISE (ABORT, 'yuufey_')
- --   END;
+--Triggers crées (se créent sans erreur dans DB browser, mais pas ici)
+CREATE TRIGGER InsertCheck
+   BEFORE INSERT ON LesInscriptions
+   WHEN (NEW.numIn NOT IN(SELECT numSp FROM LesSportifsEQ) OR NEW.numIn NOT IN(SELECT numEq FROM LesSportifsEQ) OR NEW.numEp NOT IN(SELECT numEp FROM LesEpreuves))
+   BEGIN
+        SELECT RAISE (ABORT, 'numIn/numEp non répertorié' );
+   END;
+
+CREATE TRIGGER UpdateCheck
+   BEFORE UPDATE ON LesInscriptions
+   WHEN (NEW.numIn NOT IN(SELECT numSp FROM LesSportifsEQ) OR NEW.numIn NOT IN(SELECT numEq FROM LesSportifsEQ) OR NEW.numEp NOT IN(SELECT numEp FROM LesEpreuves))
+   BEGIN
+        SELECT RAISE (ABORT, 'numIn/numEp non répertorié' );
+   END;
+
 
