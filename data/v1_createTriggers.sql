c4cad0ddde84086d5dfd465931aaec3f7c274666
SOLO-EAGLE <eagle1bluestorm@gmail.com>
2022-12-02 14:59:23 +0100

    maj triggers

--- data/v1_createTriggers.sql
+++ data/v1_createTriggers.sql
@@ -1,12 +1,12 @@
 --Triggers crées (se créent sans erreur dans DB browser, mais pas ici)
-CREATE TRIGGER InsertCheck
+CREATE TRIGGER IF NOT EXISTS InsertCheck
    BEFORE INSERT ON LesInscriptions
    WHEN (NEW.numIn NOT IN(SELECT numSp FROM LesSportifsEQ) OR NEW.numIn NOT IN(SELECT numEq FROM LesSportifsEQ) OR NEW.numEp NOT IN(SELECT numEp FROM LesEpreuves))
    BEGIN
         SELECT RAISE (ABORT, 'numIn/numEp non répertorié' );
    END;
 
-CREATE TRIGGER UpdateCheck
+CREATE TRIGGER IF NOT EXISTS UpdateCheck
    BEFORE UPDATE ON LesInscriptions
    WHEN (NEW.numIn NOT IN(SELECT numSp FROM LesSportifsEQ) OR NEW.numIn NOT IN(SELECT numEq FROM LesSportifsEQ) OR NEW.numEp NOT IN(SELECT numEp FROM LesEpreuves))
    BEGIN
